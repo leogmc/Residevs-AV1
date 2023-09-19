a20a7c3844a8391ae0579d893d3e16a1bb34d0dc
NathalieLima <nathalielima3@gmail.com>
2023-09-19 10:25:55 -0300

    Corrigindo arquivo de data.cpp

--- data.cpp
+++ data.cpp
@@ -10,17 +10,31 @@ struct Data_n {
 };
 
 bool dataValida (int dia, int mes, int ano){
+    if (ano < 1900 || ano > 2100) { return false; }
+    if (mes < 1 || mes > 12) { return false; }
+    int diasNoMes = 0;
+    if (mes == 2) {
+        diasNoMes = 
+        (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0)) ? 29 : 28;
+    }
+    else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+        diasNoMes = 30;
+    }
+    else {
+        diasNoMes = 31;
+    }
+    if (dia < 1 || dia > diasNoMes) { return false; }
 
+    return true;
 }
 
-bool dataValida2(struct Data_n data){
-
+bool dataValida(struct Data_n data){
 
     return dataValida(data.dia, data.mes, data.ano);
 }
 
 void alteraData(struct Data_n *data, int dia, int mes, int ano){
-    if(dataValida2(dia, mes, ano)) {
+    if(dataValida(dia, mes, ano)) {
         data->dia = dia;
         data->mes = mes;
         data->ano = ano;
