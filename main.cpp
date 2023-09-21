ff401b314260fd558eb4b4ceb567ad00364cceda
Lorena Andrade <lorenaandrade@gmail.com>
2023-09-21 10:34:01 -0300

    adicionando estrutura de ocorrencia e main modificado

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "roteiro.cpp"
 #include "embarca.cpp"
 #include "data.cpp"
+#include "ocorrencia.cpp"
 
 using namespace std;
 
@@ -15,31 +16,33 @@ int main(){
     vector<Embarca> embarques;// vectores principais;
     vector<Passageiro> passageiros;//vectores principais;
     vector<Roteiro> roteiros; //vectores principais;
-
+    vector<Ocorrencia> ocorrencia; //vectores principais;
     bool validar = true;
     int op;
 
     do{
         menu();//menu pricipal dentro de main.cpp
         cin >> op;
-        if ( op ==1 ){
+        if ( op ==1 )
             gestaoPassageiro(passageiros);//função dentro do arquivo passageiro.cpp
-        }else if (op == 2){
+        else if (op == 2)
             gestaoRoteiro(roteiros);//função dentro do arquivo roteiro.cpp
-           
-        }else if(op==3){
+        else if(op==3)
             gestaoEmbarque(embarques);//função dentro do arquivo embarcar.cpp
-        }else{
+        else if(op==4)
+            gestaoOcorrencia(ocorrencia);//função dentro do arquivo ocorrencia.cpp
+        else
             validar = false;
-        }
+        
     } while (validar);
 
     return 0;
 }
 void menu(){
     cout << "Bem vindo a Vans TransPaGente" <<endl;
-    cout << "1- Acessar Menu de Passageiro"<< endl;
-    cout << "2- Acessar Menu de Roteiro"<< endl; 
-    cout << "3- Acessar Menu de Embarque"<< endl; 
-    cout << "0-Sair"<< endl;
+    cout << "1. Acessar Menu de Passageiro" << endl;
+    cout << "2. Acessar Menu de Roteiro" << endl; 
+    cout << "3. Acessar Menu de Embarque" << endl; 
+    cout << "4. Gestão de Ocorrências" << endl; 
+    cout << "0. Sair"<< endl;
 }
--- ocorrencia.cpp
+++ ocorrencia.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 
 using namespace std;
-void menuEmbarque();
+void menuOcorrencia();
 
-struct Embarca{
+struct Ocorrencia{
     char realizada;
-    Data data;//data real de embarque
-    int hora;//hora real de embarque
-    int duracao;// duração real;
-    vector<Passageiro> passageiros;//Embarque[0], passageiro[i], 
+    string descricao;
+    Data data;
+    int hora;// duração real;
+    string cpf_passageiro;
     int cod_roteiro;
 }; 
 
 
-int gestaoEmbarque(vector<Embarca> &embarques){
+int gestaoOcorrencia(vector<Ocorrencia> &ocorrencia){
 
     bool validar = true;
     int op;
@@ -33,10 +33,10 @@ int gestaoEmbarque(vector<Embarca> &embarques){
                     // Alterar;
                 break;
             case 4: 
-                    // Lista;
+                    // Listar passageiro;
                 break;
             case 5:
-                    
+                    // Listar roteiro;
                 break;
             case 0:
                 validar = false;
@@ -52,10 +52,11 @@ int gestaoEmbarque(vector<Embarca> &embarques){
 }
 void menuEmbarque(){
     cout << "Bem vindo a Vans TransPaGente" << endl;
-    cout << "1-Incluir um Novo embarque"<< endl;
-    cout << "2-Excluir registro de embarque"<< endl; // pode ser por cpf
-    cout << "3-Alterar "<< endl; 
-    cout << "4-Listar todos os embarques" << endl; //listar o que os pasageiros e seus respectivos roteiro?
-    cout << "0-Sair" <<endl;
+    cout << "1. Incluir uma ocorrência"<< endl; //solicita o CPF e Código
+    cout << "2. Excluir uma ocorrência"<< endl; //solicita o CPF e Código
+    cout << "3. Alterar uma ocorrência"<< endl; //solicita o CPF e Código
+    cout << "4. Listar todas as ocorrências por Passageiro" << endl; //Solicita o CPF
+    cout << "4. Listar todas as ocorrências por Roteiro" << endl; //Solicita o Codigo
+    cout << "0. Sair" <<endl;
     cout << "Digite uma opção:";
 }
